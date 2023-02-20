cf5bf9f8e7d1491a402d24f6eb3172310cb684c1
Patrick Taylor <hptaylor@live.unc.edu>
2023-02-20 12:53:27 -0500

    more functionality in GradientSet, added docstrings/ comments, fixed style

--- Gradient.py
+++ Gradient.py
@@ -17,8 +17,24 @@ import os
 
 class Gradient:
     
+    
     def __init__(self,grad_path = None, val_path = None, gid = None):
-        
+        """
+
+        Parameters
+        ----------
+        grad_path : str, optional
+            if supplied, used to load the gradient array. The default is None.
+        val_path : str, optional
+            path to eigenvalue array. The default is None.
+        gid : str, int, optional
+            unique identifier for gradient. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
         if grad_path is not None: 
             self.garray = np.load(grad_path)
             self.nvert = self.garray.shape[0]
@@ -40,16 +56,77 @@ class Gradient:
         print(f'{self.nvert} vertices, {self.ngrad} gradients')
         return 
     
-    def compute_FC_from_fMRI(self, lh_fmri_path, rh_fmri_path, threshold = 0.03, chunk_size = 1000, symmetric = True, size = 20484, smoothing_mat = None, cos_sim= True, return_mat = True, verbose = True ):
-        
+    def compute_FC_from_fMRI(self, lh_fmri_path, rh_fmri_path, threshold = 0.03, 
+                             chunk_size = 1000, symmetric = True, size = 20484, 
+                             smoothing_mat = None, cos_sim= True, return_mat = True, 
+                             verbose = True ):
+        """
+    
+
+        Parameters
+        ----------
+        lh_fmri_path : str or list of str
+            path to left hemisphere surface-mapped 
+            fmri timeseries.
+        rh_fmri_path : str
+            path to left hemisphere surface-mapped 
+            fmri timeseries.
+        threshold : float, optional
+            minumum correlation value retained in 
+            FC matrix. The default is 0.03.
+        chunk_size : int, optional
+            number of vertices in chunk of 
+            correlation computation and 
+            thresholding to conserve memory. 
+            The default is 1000.
+        symmetric : boolean, optional
+            determines whether each acquisition's 
+            thresholded FC matrix will be averaged 
+            with its transpose to enforce symmetry. 
+            The default is True.
+        size : int, optional
+            number of surface vertices. The default 
+            is 20484.
+        smoothing_mat : scipy.sparse.csr((size,size)), 
+            optional matrix of smoothing weights to be 
+            applied to the FC matrix. The default is None.
+        cos_sim : boolean, optional
+            when true, the cosine similarity kernel is 
+            applied after artan transformation. The 
+            default is True.
+        return_mat : boolean, optional
+            if true, final FC matrix is returned. else, 
+            it is stored in fcmat attribute The default 
+            is True.
+        verbose : boolean, optional
+            if true, some description is printed. The 
+            default is True.
+
+        Returns
+        -------
+        fcmat : scipy.sparse.csr((size,size))
+            thresholded FC matrix.
+        zeromask : np.array(size)
+            array containing ones for all vertices that 
+            did not have fmri signal in every acquisition.
+
+        """
         if type(lh_fmri_path) is str:
-            fmri = io.read_functional_timeseries(lh_fmri_path, rh_fmri_path, v2s = False)
             
+            #load fMRI timeseries as np array from .gii
+            fmri = io.read_functional_timeseries(
+                        lh_fmri_path, rh_fmri_path, v2s = False)
+            
+            #mask  all vertices with no fMRI
             zeromask = uts.mask_from_timeseries_zero(fmri)
             fmri = uts.mask_medial_wall_vecs(fmri, zeromask)
             
+            #compute thresholded pairwise correlation matrix
+            fcmat = mtx.construct_FC_matrix_row_thresh(
+                        fmri, threshold = threshold, 
+                        chunk_size = chunk_size,symmetric = True)
             
-            fcmat = mtx.construct_FC_matrix_row_thresh(fmri, threshold = threshold, chunk_size = chunk_size, symmetric = True)
+            #fix potential bad data and unmask the matrix
             fcmat.data[np.isnan(fcmat.data)] = 0.0
             fcmat.eliminate_zeros()
             x = np.arange(fcmat.shape[0])
@@ -57,33 +134,47 @@ class Gradient:
             fcmat.eliminate_zeros()
             fcmat = uts.unmask_connectivity_matrix(fcmat, zeromask)
             
-        else:
-            zeromask = np.zeros(size)
-            fcmat = sparse.csr_matrix((size, size))
+        else: #we have multiple acquisitions 
             
+            zeromask = np.zeros(size)
+            #initialize empty sparse mat for average
+            fcmat = sparse.csr_matrix((size, size))
             n = 0 
+            
             for i in range(len(lh_fmri_path)):
-                fmri = io.read_functional_timeseries(lh_fmri_path[i], rh_fmri_path[i], v2s = False)
+                
+                fmri = io.read_functional_timeseries(lh_fmri_path[i], 
+                                                     rh_fmri_path[i], 
+                                                     v2s = False)
+                
                 zm = uts.mask_from_timeseries_zero(fmri)
                 zeromask+= zm 
                 fmri = uts.mask_medial_wall_vecs(fmri, zm)
-                mat = mtx.construct_FC_matrix_row_thresh(fmri, threshold = threshold, chunk_size = chunk_size, symmetric = True)
+                
+                mat = mtx.construct_FC_matrix_row_thresh(
+                        fmri, threshold = threshold, chunk_size = chunk_size,
+                        symmetric = True)
+                
                 mat.data[np.isnan(mat.data)] = 0.0
                 mat.eliminate_zeros()
                 x = np.arange(mat.shape[0])
                 mat[x,x] = 0.0 
                 mat.eliminate_zeros()
                 mat = uts.unmask_connectivity_matrix(mat, zm)
+                #add this acquisition to running sum across acquisitions
                 fcmat += mat 
                 n += 1
-            if verbose: print(f'average FC mat computed from {n} acquisitions with threshold = {threshold}')
+                
+            if verbose: print(f'''average FC mat computed 
+                              from {n} acquisitions with 
+                              threshold = {threshold}''')
             
             zeromask[zeromask>1] = 1
         
+        #load surface-based smoothing matrix
         if smoothing_mat is not None:
             
             sm = sparse.load_npz(smoothing_mat)
-            
             sm = uts.mask_connectivity_matrix(sm, zeromask)
         
         fcmat = uts.mask_connectivity_matrix(fcmat, zeromask)
@@ -92,8 +183,12 @@ class Gradient:
         fcmat.eliminate_zeros()
         fcmat.data=fcmat.data/n
         fcmat.data[fcmat.data>0.999]=0.999
+        #apply smoothing matrix
         fcmat=sm.T.dot(fcmat.dot(sm))
+        #apply arctan transform
         fcmat.data = np.arctanh(fcmat.data)
+        
+        #apply cosine similarity kernel 
         if cos_sim:
             fcmat=mtx.cosine_similarity_matrix(fcmat)
         
@@ -102,8 +197,31 @@ class Gradient:
         else:
             self.fcmat = fcmat 
             
-    def compute_grads_from_FC(self, fcmat, zeromask, num_comp = 7, savepath = None):
+    def compute_grads_from_FC(self, fcmat, zeromask, 
+                              num_comp = 7, savepath = None):
+        """
         
+
+        Parameters
+        ----------
+        fcmat : scipy.sparse.csr((size,size))
+            FC matrix.
+        zeromask : np.array(size)
+            array containing ones for all vertices that 
+            did not have fmri signal in every acquisition.
+        num_comp : int, optional
+            number of gradients to compute. The default is 7.
+        savepath : str, optional
+            path to save gradients and evals, naming convention 
+            should be /path/to/directory/{grad_ID}. The default 
+            is None.
+
+        Returns
+        -------
+        None. gradients and eigenvalues are stored in garray and 
+        varray attributes.
+
+        """
         v,w=dcp.diffusion_embed_brainspace(fcmat,num_comp)
         
         v=uts.unmask_medial_wall_vecs(v,zeromask)
@@ -115,22 +233,60 @@ class Gradient:
             np.save(savepath + '_grads', v)
             np.save(savepath + '_vals', w )
     
-    def compute_grads_from_fMRI(self, lh_fmri_path, rh_fmri_path, num_comp = 7, threshold = 0.03, chunk_size = 1000, symmetric = True, size = 20484, smoothing_mat = None, cos_sim= True, return_mat = True, savepath = None, verbose = True):
+    def compute_grads_from_fMRI(self, lh_fmri_path, rh_fmri_path, 
+                                num_comp = 7, threshold = 0.03, 
+                                chunk_size = 1000, symmetric = True, 
+                                size = 20484, smoothing_mat = None, 
+                                cos_sim= True, return_mat = True, 
+                                savepath = None, verbose = True):
+        #bottles compute_grads_from_FC and compute_FC_from_fMRI
         
-        fcmat, zeromask = self.compute_FC_from_fMRI(lh_fmri_path, rh_fmri_path, threshold, chunk_size, symmetric, size, smoothing_mat, cos_sim, True, verbose)
+        fcmat, zeromask = self.compute_FC_from_fMRI(
+                                lh_fmri_path, rh_fmri_path, threshold, 
+                                chunk_size, symmetric, size, smoothing_mat, 
+                                cos_sim, True, verbose)
         
         self.compute_grads_from_FC(self, fcmat, zeromask, num_comp, savepath)
         
     
     def evaluate(self, function, **kwargs):
- 
+        """
+        
+
+        Parameters
+        ----------
+        function : function
+            any function which takes a gradient array as first positional
+            argument followed by any number of keyword arguments.
+        **kwargs : 
+            keyword argument inputs to function.
+
+        Returns
+        -------
+        output of function
+
+        """
         return function(self.garray, **kwargs)
         
         
         
         
         
-    def compute_dispersion(self, vertex_dist_from_centroid = False):
+    def compute_dispersion(self, vertex_dist_from_centroid = False, return_result = True):
+        """
+        
+
+        Parameters
+        ----------
+        vertex_dist_from_centroid : boolean, optional
+            if true, return array of distances from each vertex to centroid 
+            of gradient embedding. The default is False.
+
+        Returns
+        -------
+        dispersion float or dist array if return_result is True/False. 
+
+        """
         centroid = np.mean(self.garray, axis = 0)
         dif = self.garray - centroid
         dists = np.square(np.linalg.norm(dif, axis = 1))
@@ -138,6 +294,12 @@ class Gradient:
         self.dispersion = disp
         if vertex_dist_from_centroid:
             self.dists_to_centroid = dists 
+        if return_result:
+            if vertex_dist_from_centroid: 
+                return dists
+            else:
+                return disp
+                
     
     def compute_explanation_ratios(self):
         ratios = np.zeros(self.varray.shape)
--- GradientSet.py
+++ GradientSet.py
@@ -11,9 +11,33 @@ import utility as uts
 import numpy as np 
 import Gradient 
 import pandas as pd 
+from sklearn.decomposition import PCA
+
 class GradientSet:
     
-    def __init__(self, pathlist = None, index = None, get_ids = True, get_vals = True, dtable = None):
+    def __init__(self, pathlist = None, index = None, 
+                 get_ids = True, get_vals = True, dtable = None):
+        """
+    
+        Parameters
+        ----------
+        pathlist : str, optional
+            liat of paths to gradient .npy files formatted as {ID}_grads.npy, 
+            if vals are available in same directory, they will also be loaded. 
+        index : any, optional 
+            user-supplied additional index on gradient set 
+        get_ids : TYPE, optional
+            DESCRIPTION. The default is True.
+        get_vals : TYPE, optional
+            DESCRIPTION. The default is True.
+        dtable : TYPE, optional
+            DESCRIPTION. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
         
         if dtable is None:
         
@@ -25,49 +49,68 @@ class GradientSet:
             glist = []
             ids = []
             if pathlist is not None: 
-                
-                for (i,p) in enumerate(pathlist): 
-                    
+                if type(pathlist[0]) == str:
+                    for (i,p) in enumerate(pathlist): 
                         
-                    if get_ids:
-                        
-                        ind = p.rfind('/') + 1
-                        
-                        gid = p[ind:-10]
-                        
-                        if get_vals:
-                            val_path = p[:-9] + 'vals.npy'
-                            g = Gradient.Gradient(p,val_path, gid)
-                        else:
-                            g = Gradient.Gradient(p,None, gid)
-                        
-                        ids.append(gid)
+                            
+                        if get_ids:
+                            
+                            ind = p.rfind('/') + 1
+                            
+                            gid = p[ind:-10]
+                            
+                            if get_vals:
+                                val_path = p[:-9] + 'vals.npy'
+                                g = Gradient.Gradient(p,val_path, gid)
+                            else:
+                                g = Gradient.Gradient(p,None, gid)
+                            
+                            ids.append(gid)
+                                
                             
                         
-                    
-                    else:
-                        if get_vals:
-                            val_path = p[:-9] + 'vals.npy'
-                            g = Gradient.Gradient(p,val_path)
                         else:
-                            g = Gradient.Gradient(p)
+                            if get_vals:
+                                val_path = p[:-9] + 'vals.npy'
+                                g = Gradient.Gradient(p,val_path)
+                            else:
+                                g = Gradient.Gradient(p)
+    
+                        glist.append(g)
                     
-                
-                    
-                    
-                    glist.append(g)
-                
-                    
-                self.dtable['grads'] = np.array(glist)
+                        
+                    self.dtable['grads'] = np.array(glist)
+    
                 self.dtable['gid'] = np.array(ids)
         
         else:
             self.dtable = dtable 
             
             
-        
+    def length(self):
+        #number of gradients in set
+        return self.dtable.shape[0]
+    
+    def g(self, ind):
+        #return grad array at index ind
+        return self.dtable['grads'][ind].garray
+    
+    def glist(self):
+        #return list of grad arrays
+        glist = []
+        for g in self.dtable.loc[:,'grads']:
+            glist.append(g.garray)
+        return glist 
+    
+    def gwhere_between(self, column, lower, upper):
+        #get gradient arrays corresponding to column value between lower and upper
+        gs = self.select_between(column, lower, upper)
+        return gs.glist()
+    
+    
     def compute_metric_on_grads(self, function, metric_name = None, *args):
-        
+        #evaluate any function whose first argument 
+        #is gradient array on all gradients in the set 
         reslist = []
         
         for grad in self.dtable['grads']:
@@ -88,16 +131,142 @@ class GradientSet:
             return reslist 
         
     def select_equal(self, column, value):
-        
+        #return new GradientSet where column field equals value
         newtable = self.dtable[self.dtable[column] == value]
         
         return GradientSet(dtable = newtable)
     
     def select_between(self, column, lower, upper):
-        
+        #return new GradientSet where lower < column field < upper
         newtable = self.dtable[self.dtable[column].between(lower, upper)]
         
         return GradientSet(dtable = newtable)
+    
+    def get_field_from_dataframe(self, dataframe, foreign_fieldname, 
+                                 native_fieldname, foreign_key = 'src_subject_id', 
+                                 native_key = 'gid'):
+        """
+        
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame() or str 
+            external dataframe or path to it containing gradient info.
+        foreign_fieldname : str
+            column name in external dataframe from which to import new data.
+        native_fieldname : str
+            column name in GradientSet dataframe as destination of new data.
+        foreign_key : str, optional
+            column in external df to find matching rows. The default is 
+            'src_subject_id'.
+        native_key : str, optional
+            column in GradientSet dataframe to search for in external df. 
+            The default is 'gid'.
+
+        Returns
+        -------
+        adds column native_fieldname to dtable with matching data from 
+        external dataframe.
+
+        """
+        if type(dataframe) == str: 
+            if dataframe[-1] == 'v' : 
+                dataframe = pd.read_csv(dataframe)
+            else: 
+                dataframe = pd.read_excel(dataframe)
+        
+        if not native_fieldname in self.dtable.columns:
+            self.dtable[native_fieldname] = np.nan
+        
+        for i in range (len(self.dtable)):
+            
+            n = self.dtable[native_key][i]
+            
+            if n in set(dataframe[foreign_key]):
+                
+                self.dtable.loc[i, native_fieldname] = dataframe[dataframe[foreign_key] == n]
+                [foreign_fieldname].to_numpy()[0]
+            
+    
+    def compute_pca_template(self, n_components = 3, **kwargs):
+        """
+        
+
+        Parameters
+        ----------
+        n_components : int, optional
+            number of components to compute. The default is 3.
+        **kwargs : 
+            future.
+
+        Returns
+        -------
+        None. computes principal dimensions of variation across all gradients in the set.
+
+        """
+        glist = []
+        if 'lower' in kwargs:
+            
+            for i,g in enumerate(self.dtable.loc[:,'grads']):
+                if kwargs['lower'] <=  self.dtable.loc[i,kwargs['column']] and self.dtable.loc[i,kwargs['column']] <= kwargs['upper']:
+                    glist.append(g)
+            
+            print(len(glist), ' grads used for template')
+        else:
+            for g in self.dtable.loc[:,'grads']:
+                glist.append(g)
+                        
+        gmat=np.zeros((glist[0].nvert,len(glist)*n_components))
+        
+        for i in range (len(glist)):
+            gmat[:,n_components*i:n_components*(i+1)]=glist[i].garray[:,:n_components]
+        pca=PCA(n_components=3)
+        pca.fit(gmat.T)
+        v=pca.components_
+        
+        self.template_grads = v.T
+        
+    def procrustes_align(self,replace = True, **kwargs):
+        """
+        
+
+        Parameters
+        ----------
+        replace : boolean, optional
+            if true, replace garrays with aligned garrays. The default is True.
+        **kwargs : 
+            future.
+
+        Returns
+        -------
+        None. computes iterative procrustes alignment to template_grads across all gradients 
+
+        """
+        garrlist = []
+        
+        for g in self.dtable['grads']:
+            garrlist.append(g.garray)
+        
+        if not hasattr(self, 'template_grads'):
+            
+            self.compute_pca_template(**kwargs)
+        
+        glist = uts.procrustes_alignment(np.array(garrlist)[:,:,:3], self.template_grads, n_iter=130, tol=1e-20, verbose=True)
+        
+        for i,g in enumerate(self.dtable['grads']):
+            
+            g.garray = glist[i]
+            
+    
+        
+        
+            
+            
+            
+        
+        
+        
+        
     
     
     
